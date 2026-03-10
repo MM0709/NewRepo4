33459519696ca6897c42608d1b63506681405fec
Mounika Mekapogu <mounika.mekapogu@westcon.com>
2026-03-10 04:15:35 -0400

    Created new SP [dbo].[Fx_Updates_Region_count_MM]

---
+++ dbo.Fx_Updates_Region_count.StoredProcedure.sql
@@ -0,0 +1,15 @@
+﻿USE [DGDB_PL]
+GO
+/****** Object:  StoredProcedure [dbo].[Fx_Updates_Region_count]    Script Date: 2/25/2026 2:13:26 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+Create OR ALter PROCEDURE [dbo].[Fx_Updates_Region_count_MM](  @group_Id INT, @Counts NVARCHAR(255))
+AS  
+BEGIN  
+	UPDATE DGDB_PL..FX_UPDATES_COUNTS SET Counts = @Counts WHERE Group_Id = @group_Id 
+END
+GO
+
